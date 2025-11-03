d53d90c64bf042baf5ac6bcf15f6bd6d140e76ad
Hugo ARNAL <hugo@hugoarnal.com>
2025-11-03 12:52:08 +0000

    feat: better tests

---
+++ tests/invalid/invalid.c
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2025
+** cs2-action
+** File description:
+** Hello world!
+*/
+
+int hello = 0;
+
+#include <stdio.h>
+
+int main ( void ) {
+
+    char c = ' ';
+
+    printf("Almost there...\n");
+
+    int a = 1;
+    
+    
+    printf("Hello world!\n");
+    
+    
+    
+    
+    return 0;
+
+    
+}
---
+++ tests/valid/valid.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2025
+** cs2-action
+** File description:
+** Hello world!
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello world!\n");
+    return 0;
+}
